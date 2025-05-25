32252a2ca41756b3af935bdc277fe2e891b80f4e
geekbrains <maxsimtorub@gmail.com>
2025-05-26 00:11:20 +0300

    Apply new .gitignore

--- C++/labwork8/build/libs/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/University/Dev/C++/labwork8/libs/googletest
-# Build directory: D:/University/Dev/C++/labwork8/build/libs/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
